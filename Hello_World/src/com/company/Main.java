6183f766ed68a7a4572c290fa467a605854d53cb
RuoxinWang <rwang0104@gmail.com>
2021-09-14 21:19:53 -0700

    checking in code for adding Primitive Data Type

---
+++ Hello_World/src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //function第一个字母小写，其他首字母大写
+        int numOfStudentsInClass = 62;
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        System.out.println("Max Value of Integer =" + max);
+        System.out.println("Min value of Integer =" + min);
+
+
+        short maxShort = Short.MAX_VALUE;
+        short minShort = Short.MIN_VALUE;
+
+        System.out.println("Max Value of Short =" + maxShort);
+        System.out.println("Min value of Short =" + minShort);
+
+        float myFloat = 5.23f;
+        float maxFloat = Float.MAX_VALUE;
+        float minFloat = Float.MIN_VALUE;
+
+        System.out.println("My Float= " + myFloat);
+        System.out.println("Max Value of Float =" + maxFloat);
+        System.out.println("Min value of Float =" + minFloat);
+
+        double myDouble = 5.23d;
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+
+        System.out.println("My Double= " + myDouble);
+        System.out.println("Max Value of Double =" + maxDouble);
+        System.out.println("Min value of Double =" + minDouble);
+
+        char ch = 'A';//unincode
+        System.out.println("My character =" + ch);
+
+        boolean bTestValue = true;
+        System.out.println("boolean value = " + bTestValue);
+        bTestValue = !bTestValue;
+        System.out.println("boolean Value = " + bTestValue);
+
+        long maxLong = Long.MAX_VALUE;
+        long minLong = Long.MIN_VALUE;
+
+        System.out.println("Max Value of Long =" + maxLong);
+        System.out.println("Min value of Long =" + minLong);
+
+        byte maxByte = Byte.MAX_VALUE;
+        byte minByte = Byte.MIN_VALUE;
+
+        System.out.println("Max Value of Byte =" + maxByte);
+        System.out.println("Min value of Byte =" + minByte);
+
+
+        System.out.println("Hello World");
+
+    }
+
+
+}
