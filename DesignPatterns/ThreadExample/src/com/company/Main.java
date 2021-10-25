178febd26331607a55fb54933b0d6b577ec0cf58
RuoxinWang <rwang0104@gmail.com>
2021-10-25 16:01:50 -0700

    Thread Example

--- DesignPatterns/ThreadExample/src/com/company/Main.java
+++ DesignPatterns/ThreadExample/src/com/company/Main.java
@@ -3,6 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    Person Ashish = new Peraon("")
+
+        Person Ashish = new Person("Ashish");
+        Person Bill = new Person("Bill");
+        Person Peter = new Person("Peter");
+        Person Mark = new Person("Mark");
+
+        Ashish.start();
+        Bill.start();
+        Peter.start();
+        Mark.start();
+
+        int counter = 15;
+        while(true){
+            System.out.println(" I am in main thread");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            counter--;
+
+        }
+
     }
 }
